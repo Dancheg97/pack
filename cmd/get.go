b183eff3c96fda13a9d097083e29fbdbd5c67201
dancheg97 <dancheg97@fmnx.su>
2023-04-29 00:32:44 +0700

    refactoring

--- cmd/get.go
+++ cmd/get.go
@@ -27,40 +27,39 @@ func Get(cmd *cobra.Command, pkgs []string) {
 	CheckErr(err)
 
 	for _, pkg := range pkgs {
-		PrepareRepo(pkg)
-		SwitchToVersion(pkg)
+		info := EjectInfo(pkg)
+		PrepareRepo(info)
+		SwitchToVersion(info)
+		GeneratePkgbuild(info)
 	}
 }
 
-func PrepareRepo(pkg string) {
-	pkgLink := "https://" + strings.Split(pkg, "@")[0]
-	pkgSplit := strings.Split(pkgLink, "/")
-	pkgName := pkgSplit[len(pkgSplit)-1]
-	err := core.SystemCallf("git clone %s %s/%s", pkgLink, CacheDir, pkgName)
-	if err != nil {
-		if !strings.Contains(err.Error(), "exit status 128") {
-			CheckErr(err)
-		}
-		fmt.Println("pulling changes")
-		err = core.SystemCallf("git -C %s/%s pull ", CacheDir, pkgName)
-		CheckErr(err)
-	}
+type PackageInfo struct {
+	Name    string
+	Link    string
+	Version string
+	Owner   string
 }
 
-func SwitchToVersion(pkg string) {
-	pkgRepo := strings.Split(pkg, "@")[0]
-	pkgSplit := strings.Split(pkgRepo, "/")
-	pkgName := pkgSplit[len(pkgSplit)-1]
+func EjectInfo(pkg string) PackageInfo {
+	link := "https://" + strings.Split(pkg, "@")[0]
+	split := strings.Split(link, "/")
+	name := split[len(split)-1]
+	owner := strings.Join(split[0:len(split)-1], "/")
+	version := ""
 	if len(strings.Split(pkg, "@")) == 1 {
 		branch, err := GetDefaultBranch(pkg)
 		CheckErr(err)
-		err = core.SystemCallf("git -C %s/%s checkout %s", CacheDir, pkgName, branch)
-		CheckErr(err)
-		return
+		version = branch
+	} else {
+		version = strings.Split(pkg, "@")[1]
 	}
-	pkgVer := strings.Split(pkg, "@")[1]
-	err := core.SystemCallf("git -C %s/%s checkout %s", CacheDir, pkgName, pkgVer)
-	CheckErr(err)
+	return PackageInfo{
+		Name:    name,
+		Link:    link,
+		Version: version,
+		Owner:   owner,
+	}
 }
 
 func GetDefaultBranch(pkg string) (string, error) {
@@ -68,4 +67,29 @@ func GetDefaultBranch(pkg string) (string, error) {
 	out, err := core.SystemCallOutf("git remote show %s | sed -n '/HEAD branch/s/.*: //p'", pkgLink)
 	CheckErr(err)
 	return strings.Trim(out, "\n"), nil
+}
+
+func PrepareRepo(i PackageInfo) {
+	err := core.SystemCallf("git clone %s %s/%s", i.Link, CacheDir, i.Name)
+	if err != nil {
+		if !strings.Contains(err.Error(), "exit status 128") {
+			CheckErr(err)
+		}
+		fmt.Println("pulling changes")
+		err = core.SystemCallf("git -C %s/%s pull ", CacheDir, i.Name)
+		CheckErr(err)
+	}
+}
+
+func SwitchToVersion(i PackageInfo) {
+	err := core.SystemCallf("git -C %s/%s checkout %s", CacheDir, i.Name, i.Version)
+	CheckErr(err)
+}
+
+func GeneratePkgbuild(i PackageInfo) {
+
+}
+
+func InstallPackage() {
+
 }
