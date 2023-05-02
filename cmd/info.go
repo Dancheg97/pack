2b57e7dda5987939df70dddc3f85485b48d1f1eb
dancheg97 <dancheg97@fmnx.su>
2023-05-02 18:52:45 +0700

    added command to recieve information about some package

--- cmd/info.go
+++ cmd/info.go
@@ -1,1 +1,50 @@
 package cmd
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"fmnx.io/dev/pack/core"
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(infoCmd)
+}
+
+var infoCmd = &cobra.Command{
+	Use:     "info",
+	Aliases: []string{"i"},
+	Short:   "🏷️  view information about package",
+	Long: `🏷️  view information about package
+
+This tool provides information about package retrieved from pacman.
+
+Example:
+pack info fmnx.io/dev/ainst`,
+	Run: Info,
+}
+
+func Info(cmd *cobra.Command, pkgs []string) {
+	if len(pkgs) != 1 {
+		RedPrint(
+			"Please, specify single arguement, provided: ",
+			color.RedString(strings.Join(pkgs, " ")),
+		)
+		os.Exit(1)
+	}
+	pkg := pkgs[0]
+	packageMapping := ReadMapping()
+	pacmanpkg, ok := packageMapping[pkg]
+	if !ok {
+		pacmanpkg = pkg
+	}
+	info, err := core.SystemCall("pacman -Qi " + pacmanpkg)
+	if err != nil {
+		RedPrint("Error: ", strings.ReplaceAll(info, "error: ", ""))
+		os.Exit(1)
+	}
+	fmt.Println(info)
+}
