c67ccf2bcd50be7b114612073cebe04cd255f837
dancheg97 <dancheg97@fmnx.su>
2023-05-09 19:50:11 +0700

    .

--- system/files.go
+++ system/files.go
@@ -1,6 +1,6 @@
 // Copyright 2023 FMNX Linux team.
 // This code is covered by GPL license, which can be found in LICENSE file.
-// Additional information could be found here: https://fmnx.io/
+// Additional information could be found on official web page: https://fmnx.io/
 // Email for contacts: help@fmnx.io
 package system
 
