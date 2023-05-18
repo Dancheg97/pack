5d599d20886ca9ab7fcae65df63b2bfbe290a1dd
dancheg97 <dancheg97@fmnx.su>
2023-05-18 15:07:08 +0700

    .

--- main.go
+++ main.go
@@ -5,23 +5,15 @@
 
 package main
 
-// Package description.
-//
-// cmd - commands, each file represents a single command
-//
-// config - CLI configuration
-//
-// git - library for accessing git functionality
-//
-// pacman - library for accessing pacman and makepkg functionality
-//
-// pack - all operations related to pack database and outputs
-//
-// prnt - utility for pretty printing
-//
-// system - library for executing system calls and file operations
-//
-// tmpl - string templates
+// Project modules overview:
+// - cmd - commands, each file in cmd directory represents a single cli command
+// - config - CLI configuration
+// - git - library for accessing git functionality
+// - pacman - library for accessing pacman and makepkg functionality
+// - pack - all operations related to pack database and outputs
+// - prnt - utility for pretty printing
+// - system - library for executing system calls and file operations
+// - tmpl - string templates
 
 import "fmnx.su/core/pack/cmd"
 
