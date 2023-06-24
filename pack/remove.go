5f3fb85f0442eceba9066b161271dc7ddb7781ab
dancheg97 <dancheg97@fmnx.su>
2023-06-24 22:36:48 +0300

    added taget header for removed package

--- pack/remove.go
+++ pack/remove.go
@@ -154,6 +154,7 @@ func rmRemote(p *RemoveParameters, pkg, email string) error {
 
 	req.Header.Add("email", email)
 	req.Header.Add("distro", p.Distro)
+	req.Header.Add("target", target)
 
 	var client http.Client
 	resp, err := client.Do(req)
